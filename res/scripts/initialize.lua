65dab1eb823ccec40fe7384dfb517da4b9cac71f
Alexander Hedlund <Alexander Hedlund>
2015-11-15 00:09:00 +0100

    Hej

--- res/scripts/initialize.lua
+++ res/scripts/initialize.lua
@@ -5,10 +5,11 @@ window = {
 	height = 600
 }
 
+scale = 6.0
 player = {
-	file = '../../res/pirat.png',
+	file = '../../res/TB1.0.png',
 	x = 350.0,
 	y = 250.0,
-	width = 100,
-	height = 100
+	width = 7 * scale,
+	height = 16 * scale
 }
