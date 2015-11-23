d013e04a0f1a34556a766b095e798e9ebaf5698a
ErikWallstrom <erik.wallstrom@live.com>
2015-11-23 15:38:18 +0100

    fixed animation bug

--- src/Main.c
+++ src/Main.c
@@ -76,7 +76,7 @@ int render(Program* program)
 		
 		if(gcomponent->images != NULL)
 		{
-			if(SDL_GetTicks() - gcomponent->animation_delay > gcomponent->animation_time)
+			if(SDL_GetTicks() - gcomponent->animation_delay > gcomponent->animation_time && SDL_GetTicks() > 1000)
 			{
 				gcomponent->animation_selected++;
 				if(gcomponent->animation_selected > array_getlength(gcomponent->images) - 1)
