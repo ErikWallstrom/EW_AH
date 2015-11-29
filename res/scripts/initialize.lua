6a400d515738909737e5faa1d2d995ef57c614be
AlexanderHedlund <alexander21@hotmail.se>
2015-11-29 20:59:52 +0100

    fml

--- res/scripts/initialize.lua
+++ res/scripts/initialize.lua
@@ -14,25 +14,21 @@ player = { graphics_component = {} }
 player.graphics_component.file = '../../res/images/TP_1.0_SpriteSheet.png'
 
 --The time that each frame shows in milliseconds
-player.graphics_component.delay = 1000
+player.graphics_component.delay = 500
 
 --Specify sprites in sprite sheet
 player.graphics_component.frames = {
 	frame1 = {
 		--X and Y position in sprite sheet
-		x = 9,
-		y = 0
+		x = 0,
+		y = 18
 	},
 	frame2 = {
-		x = 18,
+		x = 0,
 		y = 0
 	},
 	--frame3...
 }
-
---X and Y position in window (in pixels)
-player.graphics_component.x = 350.0
-player.graphics_component.y = 250.0
 
 --The scale the sprite shown in window is
 player.graphics_component.scale = 6
@@ -40,3 +36,7 @@ player.graphics_component.scale = 6
 --Width and height of each sprite in sprite sheet
 player.graphics_component.width = 9
 player.graphics_component.height = 18
+
+--X and Y position in window (in pixels)
+player.graphics_component.x = window.width / 2 - (player.graphics_component.width / 2) * player.graphics_component.scale
+player.graphics_component.y = window.height / 2 - (player.graphics_component.height / 2) * player.graphics_component.scale
