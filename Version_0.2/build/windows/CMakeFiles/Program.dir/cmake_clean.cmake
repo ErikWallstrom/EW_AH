b2d7d34f145915c7d20161310ff263f9514f63ab
ErikWallstrom <erik.wallstrom@live.com>
2016-02-24 23:20:51 +0100

    Fixed windows build

---
+++ Version_0.2/build/windows/CMakeFiles/Program.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Program.dir/src/Main.c.obj"
+  "Program.pdb"
+  "Program.exe"
+  "Program.exe.manifest"
+  "libProgram.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Program.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- Version_0.2/src/Main.c
+++ Version_0.2/src/Main.c
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#undef main
 int main(void)
 {
 	lua_State* L = luaL_newstate();
